058ec922610280fab346bb4fad7b548d61460aab
devmutinda <davidkymutinda@gmail.com>
2024-01-05 20:44:27 +0300

    update: create project function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
 import SideTab from "./components/SideTab";
-import MainTab from "./components/MainTab";
+import NoProject from "./components/NoProject"
+import CreateProject from "./components/CreateProject"
+import ViewProject from "./components/ViewProject";
+import { useState, useRef } from "react";
+
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [project, setProject] = useState({});
+  const [viewProject, setViewProject] = useState(false);
+  const [isCreateProject, setIsCreateProject] = useState(false);
+  // let project = useRef();
+  function showCreateProject(bool=true) {
+    setViewProject(false);
+    setIsCreateProject(bool);
+  }
+
+  function saveProject(newProject) {
+    setProjects((prevProjects) => {
+      return [
+        ...prevProjects,
+        newProject
+      ];
+    });
+    setIsCreateProject(false);
+  }
+
+  function showProjectDetails(selectedProject, index) {
+    // project.current = selectedProject;
+    console.log({...selectedProject, index});
+    setProject({...selectedProject, index});
+    setViewProject(true);
+  }
+
+  function saveTask(task) {
+    setProject(prev => {
+      const prevTasks = prev.tasks;
+      const tasks = [
+          ...prevTasks,
+          task,
+      ]
+
+      return { ...prev,tasks, }
+    });
+    if (setViewProject) {
+      projects[project.index] = project;
+    }
+  }
   return (
     <>
-      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <main className="h-screen my-8 flex gap-8">
-        <SideTab />
-        <MainTab />
-
+        <SideTab onViewProject={showProjectDetails} projects={projects} isCreateProject={isCreateProject} onSelect={showCreateProject}/>
+        {!isCreateProject && !viewProject  && <NoProject onSelect={showCreateProject} />}
+        {isCreateProject && !viewProject && <CreateProject onSave={saveProject}  onSelect={showCreateProject}/>}
+        {viewProject && <ViewProject updateProject={saveTask} project={project}/>}
       </main>
     </>
   );
--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -1,15 +1,15 @@
 import logo from '../assets/no-projects.png';
 
-export default function NoProject() {
+export default function NoProject({ onSelect }) {
     return (
         <div className="mt-24 text-center w-2/3">
-            <p className="flex flex-col items-center gap-1 my-4">
+            <div className="flex flex-col items-center gap-1 my-4">
                 <img className="w-16 h-16 object-contain mx-auto" src={logo} alt='Notebook image'/>
                 <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
                 <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
                 <button className="text-stone-600 hover:text-stone-950"></button>
-                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">Create new project</button>
-            </p>
+                <button onClick={onSelect} className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">Create new project</button>
+            </div>
         </div>
     );
 }
